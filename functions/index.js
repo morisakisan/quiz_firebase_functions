2b48b8d333b78da3535b18bc3b3a0b06cfbbac46
森崎真悟 <>
2023-10-18 17:30:36 +0900

    no message

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,32 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.deleteUserAccount = functions.https.onCall(async (data, context) => {
+    const userId = context.auth.uid;
+
+    // Firestoreからのデータ削除
+    const db = admin.firestore();
+    const collections = await db.listCollections();
+    const promises = [];
+
+    for (const collection of collections) {
+        const userDocs = await collection.where('uid', '==', userId).get();
+        userDocs.forEach(doc => {
+            promises.push(doc.ref.delete());
+        });
+    }
+
+    // Storageからのデータ削除
+    const storageBucket = admin.storage().bucket();
+    const userDirectory = `path/to/user/directory/${userId}/`;
+    promises.push(storageBucket.deleteFiles({ prefix: userDirectory }));
+
+    // Promiseの実行
+    await Promise.all(promises);
+
+    // FirebaseAuthからのアカウント削除
+    await admin.auth().deleteUser(userId);
+
+    return { success: true };
+});
