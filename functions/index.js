f0e23283a28ae79462fd7dc466e7077f435f25d1
morisakisan <shingo.morisaki@theport.jp>
2023-10-25 12:35:29 +0900

    A (#2)
    * no message
    
    * no message
    
    ---------
    
    Co-authored-by: 森崎真悟 <>

--- functions/index.js
+++ functions/index.js
@@ -1,50 +1,62 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 admin.initializeApp();
 
 exports.deleteUserAccount = functions.https.onCall(async (data, context) => {
-    const userId = context.auth.uid;
+  const userId = context.auth.uid;
 
-    // Firestoreからのデータ削除
-    const db = admin.firestore();
-    const collections = await db.listCollections();
-    const promises = [];
+  // Firestoreからのデータ削除
+  const db = admin.firestore();
+  const promises = [];
 
-    const topLevelCollections = await db.listCollections();
-    for (const collection of topLevelCollections) {
-        const promise = await deleteDocumentsWithUserId(collection, userId, promises);
-        promises.push(promise);
-    }
+  const topLevelCollections = await db.listCollections();
+  for (const collection of topLevelCollections) {
+    const promise = await deleteDocumentsWithUserId(
+        collection, userId, promises,
+    );
+    promises.push(promise);
+  }
 
-    // Storageからのデータ削除
-    const storageBucket = admin.storage().bucket();
-    const userDirectory = `${userId}/`;
-    promises.push(storageBucket.deleteFiles({ prefix: userDirectory }));
+  // Storageからのデータ削除
+  const storageBucket = admin.storage().bucket();
+  const userDirectory = `${userId}/`;
+  promises.push(storageBucket.deleteFiles({prefix: userDirectory}));
 
-    // Promiseの実行
-    await Promise.all(promises);
+  // Promiseの実行
+  await Promise.all(promises);
 
-    // FirebaseAuthからのアカウント削除
-    await admin.auth().deleteUser(userId);
+  // FirebaseAuthからのアカウント削除
+  await admin.auth().deleteUser(userId);
 
-    return { success: true };
+  return {success: true};
 });
 
+/**
+ * ユーザーIDに基づいて、指定されたコレクション内のドキュメントと、
+ * それらのドキュメントのサブコレクションを再帰的に削除します。
+ *
+ * @async
+ * @function
+ * @param {firebase.firestore.CollectionReference} collectionRef
+ * @param {string} userId - 削除するドキュメントのユーザーID。
+ * @return {Promise<Array<firebase.firestore.WriteResult>>} 削除操作のPromiseの配列。
+ */
 async function deleteDocumentsWithUserId(collectionRef, userId) {
-    const userDocs = await collectionRef.where('uid', '==', userId).get();
-    const promises = [];
+  const userDocs = await collectionRef.where("uid", "==", userId).get();
+  const promises = [];
 
-    for (const doc of userDocs.docs) {
-        const subCollections = await doc.ref.listCollections();
-        for (const subCollection of subCollections) {
-            const allDocsInSubCollection = await subCollection.get();
-            for (const subDoc of allDocsInSubCollection.docs) {
-                promises.push(subDoc.ref.delete());
-            }
-            promises.push(deleteDocumentsWithUserId(subCollection, userId)); // サブコレクションのサブコレクションを再帰的に処理
-        }
-        promises.push(doc.ref.delete()); // 親ドキュメントを削除
+  for (const doc of userDocs.docs) {
+    const subCollections = await doc.ref.listCollections();
+    for (const subCollection of subCollections) {
+      const allDocsInSubCollection = await subCollection.get();
+      for (const subDoc of allDocsInSubCollection.docs) {
+        promises.push(subDoc.ref.delete());
+      }
+      // サブコレクションのサブコレクションを再帰的に処理
+      promises.push(deleteDocumentsWithUserId(subCollection, userId));
     }
+    promises.push(doc.ref.delete()); // 親ドキュメントを削除
+  }
 
-    return Promise.all(promises);
+  return Promise.all(promises);
 }
